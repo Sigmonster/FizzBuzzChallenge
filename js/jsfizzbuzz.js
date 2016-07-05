e2d6eb86d9b84eb55605957ef0765a46d9e0b56e
AJ Sigmon <alexandersigmon@gmail.com>
2016-07-05 14:29:44 -0400

    initial commit, linked css & js directories

---
+++ js/jsfizzbuzz.js
@@ -0,0 +1,13 @@
+//js fizzbuzz challenge
+/*
+var mytest
+mytest=10;
+document.write(mytest);
+*/
+
+var numbers = [];
+
+for (var i =0; i<100; i++){
+	numbers[i] = i + 1;
+}
+	document.write(numbers.valueOf())
